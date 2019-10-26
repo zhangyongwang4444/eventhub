08f24040b2bc43e521b888efa6b36b8606f58ea1
zhangyongwang <20225614@qq.com>
2019-10-26 13:40:30 +0800

    优化源码 优化测试用例

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,34 @@
 class EventHub {
-    cache = {}
-    // {
-    //  '楚天都市报':[fn1,fn2,fn3],
-    //  '羊城晚报':[fn1,fn2,fn3]
-    // }
+    private cache = {}
     on(eventName, fn) {
-        // 把 fn 推进 this.cache[eventName] 数组
-        // eventName = 楚天，fn
         this.cache[eventName] = this.cache[eventName] || []
         this.cache[eventName].push(fn)
     }
     emit(eventName, data?) {
-        // 把 this.cache[eventName] 数组里面的 fn 全部 依次调用
         (this.cache[eventName] || []).forEach(fn => fn(data));
     }
     off(eventName, fn) {
-        // 把 fn 弹出 this.cache[eventName] 数组
-        this.cache[eventName] = this.cache[eventName] || []
-        let index = undefined
-        for (let i = 0; i < this.cache[eventName].length; i++) {
-            if (this.cache[eventName][i] === fn) {
-                index = i
-                break
-            }
-        }
-        if (index === undefined) {
-            return
-        } else {
-            this.cache[eventName].splice(index, 1)
-        }
+        let index = indexOf(this.cache[eventName], fn)
+        if (index === -1) return
+        this.cache[eventName].splice(index, 1)
     }
 }
 
 export default EventHub
 
+/**
+ * 帮助函数 indexOf
+ * @param array 
+ * @param item 
+ */
+function indexOf(array, item) {
+    if (array === undefined) return -1
+    let index = -1
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === item) {
+            index = i
+            break
+        }
+    }
+    return index
+}
--- test/index.ts
+++ test/index.ts
@@ -1,29 +1,39 @@
 import EventHub from "../src/index"
 
-const eventHub = new EventHub()
+const test1 = (message) => {
+    const eventHub = new EventHub()
+    console.assert(eventHub instanceof Object === true, "eventHub 是个对象")
+    console.log(message)
+}
+const test2 = (message) => {
+    const eventHub = new EventHub()
+    let called = false
+    eventHub.on("xxx", (y) => {
+        called = true
+        console.assert(y === "今天天气晴转多云")
+    })
+    eventHub.emit("xxx", "今天天气晴转多云");
+    setTimeout(() => {
+        console.assert(called === true)
+        console.log(message)
+    }, 1000)
+}
+const test3 = (message) => {
+    const eventHub = new EventHub()
+    let called = false
+    const fn1 = () => { called = true }
+    eventHub.on("yyy", fn1)
+    eventHub.off("yyy", fn1)
+    eventHub.emit("yyy");
+    setTimeout(() => {
+        console.assert(called === false)
+        console.log(message)
+    }, 1000)
+}
 
-console.assert(eventHub instanceof Object === true, "eventHub 是个对象")
-
-// on emit 
-let called = false
-eventHub.on("xxx", (y) => {
-    called = true
-    console.log("called:" + called)
-    console.log(y)
-    console.assert(y === "今天天气晴转多云")
-})
-
-eventHub.emit("xxx", "今天天气晴转多云");
+test1('EventHub 可以创建对象')
+test2('.on 了之后 .emit 会触发 .on的函数')
+test3('.off 有用')
 
 
-
-const eventHub2 = new EventHub()
-let called2 = false
-const fn1 = () => { called2 = true }
-eventHub2.on("yyy", fn1)
-eventHub2.off("yyy", fn1)
-eventHub2.emit("yyy");
-setTimeout(() => {
-    console.log(called2)
-}, 1000)
 
